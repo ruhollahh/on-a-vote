8f434d6ebdee974bdab13ac4b978459b955618b5
ruhollahh <ruhollahh01@gmail.com>
2022-04-23 19:53:57 +0430

    create question

--- src/backend/routes/question.ts
+++ src/backend/routes/question.ts
@@ -1,8 +1,21 @@
 import * as trpc from "@trpc/server";
 import { prisma } from "@/backend/utils/prisma";
+import { z } from "zod";
 
-export const questionRouter = trpc.router().query("getAll", {
-  async resolve() {
-    return await prisma.pollQuestion.findMany();
-  },
-});
+export const questionRouter = trpc
+  .router()
+  .query("getAll", {
+    async resolve() {
+      return await prisma.pollQuestion.findMany();
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      question: z.string().min(5).max(600),
+    }),
+    async resolve({ input }) {
+      return await prisma.pollQuestion.create({
+        data: { question: input.question },
+      });
+    },
+  });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,47 @@
 import { trpc } from "@/backend/utils/trpc";
 import type { NextPage } from "next";
+import { useState } from "react";
+
+const QuestionForm = () => {
+  const [question, setQuestion] = useState("");
+  const client = trpc.useContext();
+  const { mutate, error, isLoading } = trpc.useMutation("question.create", {
+    async onSuccess() {
+      await client.invalidateQueries(["question.getAll"]);
+      setQuestion("");
+    },
+  });
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        mutate({ question });
+      }}
+    >
+      <label htmlFor="question">New Question:</label>
+      <input
+        id="question"
+        disabled={isLoading}
+        onChange={(e) => setQuestion(e.target.value)}
+        value={question}
+      />
+      {error && <p>{error.message}</p>}
+    </form>
+  );
+};
 
 const Home: NextPage = () => {
   const { data, isLoading } = trpc.useQuery(["question.getAll"]);
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  console.log(data);
   return (
     <div>
-      <p>got it</p>
+      {data?.map((q) => (
+        <p key={q.id}>{q.question}</p>
+      ))}
+      <QuestionForm />
     </div>
   );
 };
