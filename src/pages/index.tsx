97d8f1d593a53dee681dfb835e1c85c7f92f937f
ruhollahh <ruhollahh01@gmail.com>
2022-04-23 15:41:22 +0430

    prisma generate

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Heading } from "@chakra-ui/react";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <Heading>Hello from chakra-ui</Heading>;
+};
+
+export default Home;
