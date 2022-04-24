587d9c511316bfe8bd132700b616b4730f7c7059
ruhollahh <ruhollahh01@gmail.com>
2022-04-24 23:23:16 +0430

    auth db persist and put userId in session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { prisma } from "@/backend/utils/prisma";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
     maxAge: 24 * 60 * 60,
@@ -12,6 +15,18 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.userId = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.userId = token.userId;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
